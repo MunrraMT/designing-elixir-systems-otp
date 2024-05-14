363393dd7d19a8642e2518f032ca466e14830c99
MunrraMT <dev.armsantos@gmail.com>
2024-05-14 18:14:58 -0400

    cap4 - Name Concepts with Functions

--- cap4/mastery/lib/mastery/core/question.ex
+++ cap4/mastery/lib/mastery/core/question.ex
@@ -1,6 +1,28 @@
 defmodule Mastery.Core.Question do
   defstruct ~w[asked substitutions template]a
 
+  alias Mastery.Core.Template
+
+  def new(%Template{} = template) do
+    template.generators
+    |> Enum.map(&build_substitution/1)
+    |> evaluate(template)
+  end
+
+  defp compile(template, substitutions) do
+    template.compiled
+    |> Code.eval_quoted(assigns: substitutions)
+    |> elem(0)
+  end
+
+  defp evaluate(substitutions, template) do
+    %__MODULE__{
+      asked: compile(template, substitutions),
+      substitutions: substitutions,
+      template: template
+    }
+  end
+
   defp build_substitution({name, choices_or_generator}) do
     {name, choose(choices_or_generator)}
   end
